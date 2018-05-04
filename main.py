6793839a7d8d606d68621fa4a32eda2afdc41c24
DESKTOP-0B91SGH\Augusto <augustovanderley@gmail.com>
2018-05-04 20:57:13 -0300

    refatorando e organizando métodos

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import random
 import shutil
 import re
 
+def geraListasDeAcordoComTags():
+    listaMusicasRapidas = [];
+    listaMusicasLentas = [];
+    for f in os.listdir("Forró"):
+        audioFile = eyed3.load("Forró/" + f)
+        tag = audioFile.tag
+        ##100 pra cima é rápido
+        #if(tag.genre is not None and tag.genre.name == "Xote"):
+        if(tag.bpm > 100):
+            listaMusicasRapidas.append(f);
+        else:
+            listaMusicasLentas.append(f)
+    return listaMusicasRapidas, listaMusicasLentas
 
 def retornaNomeArquivoNoPadrao(indiceMusicaAtual, indiceBlocoAtual, musicaAleatoria ):
     audioFile = eyed3.load("Forró/" + musicaAleatoria)
@@ -17,56 +30,46 @@ def retornaNomeArquivoNoPadrao(indiceMusicaAtual, indiceBlocoAtual, musicaAleato
             indiceBlocoAtual + " (" + str(tag.bpm).zfill(3) + "bpm)(" + tag.genre.name +") " +
             tag.artist.replace("/", ",")+ " - " + tag.title +".mp3" )
    
-
-
-listaMusicasRapidas = [];
-listaMusicasLentas = [];
-for f in os.listdir("Forró"):
-    audioFile = eyed3.load("Forró/" + f)
-    tag = audioFile.tag
-    ##100 pra cima é rápido
-    #if(tag.genre is not None and tag.genre.name == "Xote"):
-    if(tag.bpm > 100):
-        listaMusicasRapidas.append(f);
+def criaDiretorioDestino(nomePlaylist):
+    if not os.path.exists(nomePlaylist):
+        os.mkdir(nomePlaylist)
     else:
-        listaMusicasLentas.append(f)
-        
-print(listaMusicasRapidas)
-print(listaMusicasLentas)
+        shutil.rmtree(nomePlaylist)
+        os.mkdir(nomePlaylist)
+    return
+
+def geraPlaylistDestino(listaMusicasRapidas, listaMusicasLentas, nomePlaylist):
+    indiceMusicaAtual = 1;
+    indiceBlocoAtual = 'A';
+    random.shuffle(listaMusicasRapidas)
+    random.shuffle(listaMusicasLentas)
+    musicaRapida = True;
+    for x in range(0,3):
+        for y in range(0,2):
+            if(musicaRapida == True):
+                if(listaMusicasRapidas):
+                    musicaAleatoria = listaMusicasRapidas.pop();
+                else:
+                    musicaAleatoria = None;
+            else:
+                if(listaMusicasLentas):
+                    musicaAleatoria = listaMusicasLentas.pop();
+                else:
+                    musicaAleatoria  = None;
+            if(musicaAleatoria is not None):
+                nomeMusicaASerAdicionada = retornaNomeArquivoNoPadrao(indiceMusicaAtual, indiceBlocoAtual, musicaAleatoria)
+                shutil.copy("Forró/" + musicaAleatoria, nomePlaylist +"/" + nomeMusicaASerAdicionada)
+            indiceMusicaAtual+= 1;
+        musicaRapida =  not musicaRapida;
+        indiceBlocoAtual = chr(ord(indiceBlocoAtual) + 1);
+    return
+
+
+listaMusicasRapidas, listaMusicasLentas = geraListasDeAcordoComTags()
 nomePlaylist="playlistMaio"
-if not os.path.exists(nomePlaylist):
-    os.mkdir(nomePlaylist)
-else:
-    shutil.rmtree(nomePlaylist)
-    os.mkdir(nomePlaylist)
-qntBlocos = 3;
-indiceMusicaAtual = 1;
-indiceBlocoAtual = 'A';
-random.shuffle(listaMusicasRapidas)
-random.shuffle(listaMusicasLentas)
-musicaRapida = True;
-for x in range(0,3):
-    #blocox
-    for y in range(0,2):
-        if(musicaRapida == True):
-            if(listaMusicasRapidas):
-                musicaAleatoria = listaMusicasRapidas.pop();
-            else:
-                musicaAleatoria = None;
-        else:
-            if(listaMusicasLentas):
-                musicaAleatoria = listaMusicasLentas.pop();
-                print("Lista")
-                print(listaMusicasLentas)
-                print(musicaAleatoria)
-            else:
-                musicaAleatoria  = None;
-        if(musicaAleatoria is not None):
-            nomeMusicaASerAdicionada = retornaNomeArquivoNoPadrao(indiceMusicaAtual, indiceBlocoAtual, musicaAleatoria)
-            shutil.copy("Forró/" + musicaAleatoria, nomePlaylist +"/" + nomeMusicaASerAdicionada)
-        indiceMusicaAtual+= 1;
-    musicaRapida =  not musicaRapida;
-    indiceBlocoAtual = chr(ord(indiceBlocoAtual) + 1);
+criaDiretorioDestino(nomePlaylist)
+geraPlaylistDestino(listaMusicasRapidas, listaMusicasLentas, nomePlaylist)
+
 
 #usar random.sample(listaMusicasRapidas, k) ou shuffle + pop
 
